723c76cf95c4765cff4542d5f82450b66c4b9cd3
Vinícius Gabriel <viniciusgsa99@gmail.com>
2023-03-22 19:33:03 -0300

    Requisito 4 concluído. Implementando função is_anagram que faz a comparação de duas strings e retorna se elas são ou não anagramas.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,47 @@
+def merge_sort(string):
+    if len(string) <= 1:
+        return string
+
+    mid = len(string) // 2
+    left = merge_sort(string[:mid])
+    right = merge_sort(string[mid:])
+
+    return merge(string, left, right)
+
+
+def merge(string_merged, left, right):
+    left_idx, right_idx = 0, 0
+
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] <= right[right_idx]:
+            string_merged[left_idx + right_idx] = left[left_idx]
+            left_idx += 1
+        else:
+            string_merged[left_idx + right_idx] = right[right_idx]
+            right_idx += 1
+
+    for left_idx in range(left_idx, len(left)):
+        string_merged[left_idx + right_idx] = left[left_idx]
+
+    for right_idx in range(right_idx, len(right)):
+        string_merged[left_idx + right_idx] = right[right_idx]
+
+    return string_merged
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui"""
-    raise NotImplementedError
+
+    first_ordened = ''
+    second_ordened = ''
+
+    for letter in merge_sort(list(first_string.lower())):
+        first_ordened += letter
+    for letter in merge_sort(list(second_string.lower())):
+        second_ordened += letter
+
+    return (
+        first_ordened,
+        second_ordened,
+        True if first_ordened == second_ordened and
+        len(first_ordened) > 0 and len(first_ordened) > 0 else False
+    )
