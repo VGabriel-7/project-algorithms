bf45ee9dda67d0162aee7eda84c1953ca95e23e9
Vinícius Gabriel <viniciusgsa99@gmail.com>
2023-03-22 15:56:39 -0300

    Requisito 1 conluído. Implementando função study_schedule que busca o número de estudantes que estudam no mesmo horário através de um parametro (target_time).

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        counter = 0
+        for period in permanence_period:
+            if target_time >= period[0] and target_time <= period[1]:
+                counter += 1
+
+        return counter
+    except TypeError:
+        return None
